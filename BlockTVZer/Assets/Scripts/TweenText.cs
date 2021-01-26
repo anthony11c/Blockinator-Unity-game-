c655feef438aeea98c474d7f4c7dec936472f026
anthony11c <anthony11c@live.com>
2021-01-26 22:16:04 +0100

    Dodan title igre i animacija na njega

---
+++ BlockTVZer/Assets/Scripts/TweenText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenText : MonoBehaviour
+{
+    public float tweenTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Tween();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Tween()
+    {
+        LeanTween.cancel(gameObject);
+
+        transform.localScale = Vector3.one;
+
+        LeanTween.scale(gameObject, Vector3.one * 3, tweenTime).setEasePunch();
+    }
+}
