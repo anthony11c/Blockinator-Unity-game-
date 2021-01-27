5519cbc6e98aa8a630da2588b7e3599449799279
domi3011 <77245689+domi3011@users.noreply.github.com>
2021-01-27 15:41:29 +0100

    Credits + MenuLoader skripta

---
+++ BlockTVZer/Assets/Scripts/MenuLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLoader : MonoBehaviour
+{
+    public string Credits;
+    public string MainMenu;
+ 
+     public void Play()
+    {
+        GetComponent<Animation>();
+        StartCoroutine(LoadAfterAnim());
+    }
+
+    public IEnumerator LoadAfterAnim()
+    {
+        yield return new WaitForSeconds(05);
+        SceneManager.LoadScene(0);
+    }
+}
