17503867e7ab906e6c370c61359da715313dca4f
Antonijo <66428056+anthony11c@users.noreply.github.com>
2021-01-27 17:17:12 +0100

    Prijelaz sa creditsa na main menu

---
+++ BlockTVZer/Assets/Scripts/LoadMainScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadMainScene : MonoBehaviour
+{
+    public void LoadNewScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
